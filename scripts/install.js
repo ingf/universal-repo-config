0bd7eacf880d1900b7ad0729cd477211f7a516a0
mark <mark@markdeiMac.local>
2021-09-21 18:23:41 +0800

    选择模板

--- scripts/install.js
+++ scripts/install.js
@@ -1,47 +1,70 @@
+/*
+ * @Description: 
+ * @Author: Mark
+ * @Date: 2021-09-18 09:59:41
+ * @LastEditors: Mark
+ * @LastEditTime: 2021-09-21 18:22:58
+ */
 const fs = require('fs')
 const path = require('path')
+const inquirer = require('inquirer')
 
-const packagePath = path.join(process.cwd(), '../../package.json')
-const package = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
-
-if (package.devDependencies['universal-repo-config']) {
-  return
-}
-
-const projectDir = path.join(process.cwd(), '../..')
-const fileList = [
-  '.eslintrc.js',
-  '.prettierrc.js',
-  '.stylelintrc.js',
-  '.gitignore',
-  '.editorconfig',
-]
-
-const appendFile = (src, dist) => {
-  fs.readFile(dist, 'utf8', function(err, result) {
-    if (err || !result) {
-      fs.writeFileSync(dist, fs.readFileSync(src))
+  ; (async () => {
+    const packagePath = path.join(process.cwd(), '../../package.json')
+    const package = JSON.parse(fs.readFileSync(packagePath, 'utf8'))
+    if (package.devDependencies['universal-repo-config']) {
       return
     }
-    const comment = dist.indexOf('.js') > -1 ? '//' : '#'
-    const lines = result.split(/\n/)
-    const content = lines
-      .map(function(line) {
-        return comment + line
-      })
-      .join('\n')
-    const currentContent = fs.readFileSync(src, 'utf8')
-    fs.writeFile(dist, content + '\n\n' + currentContent, 'utf8', function(
-      err
-    ) {
-      if (err) throw err
-      console.log(dist + ' has been saved!')
-    })
-  })
-}
+    const projectDir = path.join(process.cwd(), '../..')
 
-fileList.map(fileName => {
-  const src = path.join(__dirname, '../template/', `${fileName}-tpl`)
-  const dist = `${projectDir}/${fileName}`
-  appendFile(src, dist)
-})
+    const answers = await inquirer.prompt([{
+      name: 'type',
+      type: 'list',
+      message: 'Please choose a language to use',
+      choices: [
+        { name: 'React+JavaScript', value: 'react-js' },
+        { name: 'React+TypeScript', value: 'react-ts' },
+        { name: 'Vue+TypeScript', value: 'vue-ts' },
+        { name: 'Vue+JavaScript', value: 'vue-js' },
+        { name: 'Node+TypeScript', value: 'node-ts' },
+        { name: 'JavaScript', value: 'js' }
+      ],
+    }])
+
+    const fileList = [
+      '.eslintrc.js',
+      '.prettierrc.js',
+      '.stylelintrc.js',
+      '.gitignore',
+      '.editorconfig',
+    ]
+
+    const appendFile = (src, dist) => {
+      fs.readFile(dist, 'utf8', function (err, result) {
+        if (err || !result) {
+          fs.writeFileSync(dist, fs.readFileSync(src))
+          return
+        }
+        const comment = dist.indexOf('.js') > -1 ? '//' : '#'
+        const lines = result.split(/\n/)
+        const content = lines
+          .map(function (line) {
+            return comment + line
+          })
+          .join('\n')
+        const currentContent = fs.readFileSync(src, 'utf8')
+        fs.writeFile(dist, content + '\n\n' + currentContent, 'utf8', function (
+          err
+        ) {
+          if (err) throw err
+          console.log(dist + ' has been saved!')
+        })
+      })
+    }
+
+    fileList.map(fileName => {
+      const src = path.join(__dirname, '../template/', `./${answers.type}/${fileName}-tpl`)
+      const dist = `${projectDir}/${fileName}`
+      appendFile(src, dist)
+    })
+  })()
